190cbbfc61c0398e95d17ef2c8cd93a14dc433e9
Katrin Valge <katrin.valge@mail.ee>
2022-11-11 14:43:35 +0200

    Optimized method names.

--- src/main/java/com/example/boardgamesjavaspring/validation/ValidationService.java
+++ src/main/java/com/example/boardgamesjavaspring/validation/ValidationService.java
@@ -15,7 +15,7 @@ public class ValidationService {
     private ProductRepository productRepository;
 
     public String productExist(ProductRequest request) {
-        Product product = productRepository.findByProductNameIgnoreCase(request.getProductName());
+        Product product = productRepository.findByProductName(request.getProductName());
         if (product == null) {
             return request.getProductName() + " is added successfully";
         } else {
@@ -25,7 +25,7 @@ public class ValidationService {
     }
 
     public String NoSuchProductExists(String name) {
-        Product product = productRepository.findByProductNameIgnoreCase(name);
+        Product product = productRepository.findByProductName(name);
         if (product != null) {
             return "Operation successfully completed!";
         } else {
