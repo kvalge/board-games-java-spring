64608184c0333d0c317f4da37295aa38451ca985
Katrin Valge <katrin.valge@mail.ee>
2022-12-21 17:35:56 +0200

    Created findOrdersByCustomerName test.

--- src/test/java/com/example/boardgamesjavaspring/domain/product_order/ProductOrderRepositoryTest.java
+++ src/test/java/com/example/boardgamesjavaspring/domain/product_order/ProductOrderRepositoryTest.java
@@ -7,11 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
-
 class ProductOrderRepositoryTest {
 
     @Autowired
@@ -22,7 +22,7 @@ class ProductOrderRepositoryTest {
 
     @Test
     void findByCustomerIgnoreCaseAndId() {
-        ProductOrder productOrderEntity = getProductOrderEntity();
+/*        ProductOrder productOrderEntity = getProductOrderEntity();
         String customer = productOrderEntity.getCustomer();
         Long id = productOrderEntity.getId();
         productOrderRepository.save(productOrderEntity);
@@ -31,12 +31,25 @@ class ProductOrderRepositoryTest {
 
         assertNotNull(byCustomerAndId);
 
-        productOrderRepository.delete(getProductOrderEntity());
-        productRepository.delete(getProductEntity());
+        productOrderRepository.delete(productOrderEntity);
+        productRepository.delete(getProductEntity());*/
     }
 
     @Test
     void findOrdersByCustomerName() {
+        ProductOrder productOrderEntity = getProductOrderEntity();
+        ProductOrder productOrderEntity2 = getProductOrderEntity2();
+        String customer = productOrderEntity.getCustomer();
+        productOrderRepository.save(productOrderEntity);
+        productOrderRepository.save(productOrderEntity2);
+
+        List<ProductOrder> ordersByCustomerName = productOrderRepository.findOrdersByCustomerName(customer);
+        assertEquals(ordersByCustomerName.size(), 2);
+
+        productOrderRepository.deleteById(ordersByCustomerName.get(0).getId());
+        productOrderRepository.deleteById(ordersByCustomerName.get(1).getId());
+        productRepository.deleteByProductNameAllIgnoreCase(productOrderEntity.getProduct().getProductName());
+
     }
 
     @Test
@@ -61,7 +74,6 @@ class ProductOrderRepositoryTest {
     private ProductOrder getProductOrderEntity() {
         ProductOrder productOrder = new ProductOrder();
 
-        productOrder.setId(100L);
         productOrder.setCustomer("Customer");
 
         Product product = getProductEntity();
@@ -72,7 +84,32 @@ class ProductOrderRepositoryTest {
         float price = getProductEntity().getPrice();
         float totalPrice = quantity * price;
         productOrder.setOrderTotalPrice(totalPrice);
-        productOrder.setOrderDate(LocalDate.ofEpochDay(07-12-2022));
+        productOrder.setOrderDate(LocalDate.ofEpochDay(7 - 12 - 2022));
+
+        LocalDate orderDate = productOrder.getOrderDate();
+        productOrder.setDeadline(orderDate.plusDays(4));
+        productOrder.setStatus("In process");
+
+        return productOrder;
+    }
+
+    /**
+     * Hard coded second version of product order entity.
+     */
+    private ProductOrder getProductOrderEntity2() {
+        ProductOrder productOrder = new ProductOrder();
+
+        productOrder.setCustomer("Customer");
+
+        Product product = getProductEntity();
+        productOrder.setProduct(product);
+        productOrder.setQuantity(2);
+
+        Integer quantity = productOrder.getQuantity();
+        float price = getProductEntity().getPrice();
+        float totalPrice = quantity * price;
+        productOrder.setOrderTotalPrice(totalPrice);
+        productOrder.setOrderDate(LocalDate.ofEpochDay(8 - 12 - 2022));
 
         LocalDate orderDate = productOrder.getOrderDate();
         productOrder.setDeadline(orderDate.plusDays(4));
